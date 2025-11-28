96b10916e07bb49303db142a2cb95e2db51bb38e
ShuliDev <patrick.alexander.2003@hotmail.com>
2025-11-28 17:53:43 -0300

    Forms

--- tiendaOnlineApp/urls.py
+++ tiendaOnlineApp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('producto/<int:pk>/', views.producto_detalle, name='producto_detalle'),
+    path('solicitar/<int:producto_id>/', views.pedido_solicitud, name='pedido_solicitud'),
+    path('seguimiento/<str:token>/', views.pedido_seguimiento, name='pedido_seguimiento'),
 ]
--- tiendaOnlineApp/views.py
+++ tiendaOnlineApp/views.py
@@ -1,7 +1,41 @@
+def pedido_seguimiento(request, token):
+	pedido = get_object_or_404(Pedido, token=token)
+	imagenes = pedido.images.all()
+	return render(request, 'pedido_seguimiento.html', {
+		'pedido': pedido,
+		'imagenes': imagenes,
+	})
+from django import forms
+from .models import Pedido
+class PedidoForm(forms.ModelForm):
+	class Meta:
+		model = Pedido
+		fields = ['cliente_nombre', 'email', 'telefono', 'descripcion', 'fecha_necesaria']
+		widgets = {
+			'fecha_necesaria': forms.DateInput(attrs={'type': 'date'}),
+		}
+
+def pedido_solicitud(request, producto_id):
+	producto = get_object_or_404(Product, pk=producto_id)
+	if request.method == 'POST':
+		form = PedidoForm(request.POST, request.FILES)
+		if form.is_valid():
+			pedido = form.save(commit=False)
+			pedido.producto = producto
+			pedido.plataforma = 'web'
+			pedido.estado = 'solicitado'
+			pedido.estado_pago = 'pendiente'
+			pedido.save()
+			# TODO: manejar imágenes de referencia
+			return render(request, 'pedido_exito.html', {'pedido': pedido})
+	else:
+		form = PedidoForm()
+	return render(request, 'pedido_form.html', {'form': form, 'producto': producto})
 from django.shortcuts import render
 
 
 from .models import Product, Category
+from django.shortcuts import get_object_or_404
 
 def index(request):
 	categoria_slug = request.GET.get('categoria')
@@ -13,4 +47,12 @@ def index(request):
 		'productos': productos,
 		'categorias': categorias,
 		'categoria_seleccionada': categoria_slug,
+	})
+
+def producto_detalle(request, pk):
+	producto = get_object_or_404(Product, pk=pk)
+	imagenes = producto.images.all()
+	return render(request, 'producto_detalle.html', {
+		'producto': producto,
+		'imagenes': imagenes,
 	})
