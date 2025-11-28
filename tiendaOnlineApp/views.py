7d4ae250b671afc0f5fb9a6db032577494ad820d
ShuliDev <patrick.alexander.2003@hotmail.com>
2025-11-28 17:40:41 -0300

    Pestaña básica  para mostrar catálogo

--- tiendaOnlineApp/views.py
+++ tiendaOnlineApp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 
+from .models import Product, Category
+
 def index(request):
-	"""Página de inicio (catálogo placeholder)."""
-	return render(request, 'index.html')
+	categoria_slug = request.GET.get('categoria')
+	categorias = Category.objects.all()
+	productos = Product.objects.all()
+	if categoria_slug:
+		productos = productos.filter(category__slug=categoria_slug)
+	return render(request, 'index.html', {
+		'productos': productos,
+		'categorias': categorias,
+		'categoria_seleccionada': categoria_slug,
+	})
